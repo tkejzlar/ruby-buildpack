02b4644155614e3ac08defa56ae0d6d32f68cf24
Dave Goddard <dave@goddard.id.au>
2017-08-25 08:33:27 -0400

    unfocus
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- src/ruby/integration/deploy_a_rails51_app_test.go
+++ src/ruby/integration/deploy_a_rails51_app_test.go
@@ -19,7 +19,7 @@ var _ = Describe("Rails 5.1 (Webpack/Yarn) App", func() {
 		app.SetEnv("BP_DEBUG", "1")
 	})
 
-	FIt("Installs node6 and runs", func() {
+	It("Installs node6 and runs", func() {
 		PushAppAndConfirm(app)
 		Expect(app.Stdout.String()).To(ContainSubstring("Installing node 6."))
 
@@ -27,8 +27,8 @@ var _ = Describe("Rails 5.1 (Webpack/Yarn) App", func() {
 		Eventually(func() string { return app.Stdout.String() }, 10*time.Second).Should(ContainSubstring(`Started GET "/" for`))
 
 		By("Make sure supply does not change BuildDir", func() {
-			Expect(app.Stdout.String()).To(ContainSubstring("BuildDir Checksum Before Supply: b3d19453a33206783c48720e172bf019"))
-			Expect(app.Stdout.String()).To(ContainSubstring("BuildDir Checksum After Supply: b3d19453a33206783c48720e172bf019"))
+			Expect(app.Stdout.String()).To(ContainSubstring("BuildDir Checksum Before Supply: 5d823d48d154ee2622e8cf8c2fb21ff7"))
+			Expect(app.Stdout.String()).To(ContainSubstring("BuildDir Checksum After Supply: 5d823d48d154ee2622e8cf8c2fb21ff7"))
 		})
 	})
 })
